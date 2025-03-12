0cec34e2387f5723727ca321efa64126e75b12ea
Isaac Vetter <isaac@epic.com>
2025-03-12 08:23:58 -0500

    add 1.0.0-ballot
    from review of 1.0.1 publication

--- history-data.js
+++ history-data.js
@@ -20,7 +20,7 @@ var pageJSON = `{
 			"status": "trial-use",
 			"sequence": "STU1",
 			"fhirversion": "4.0.1",
-			"date": "2025-01-30",
+			"date": "2025-03-12",
 			"desc": "CDS Hooks Library STU1 Errata Publication",
 			"changes": "Move specification from bespoke to HL7 FHIR publishing mechanism."
 		},
@@ -40,6 +40,16 @@ var pageJSON = `{
 				<li><a href='https://cds-hooks.hl7.org/hooks/encounter-discharge/STU1/encounter-discharge/'>Encounter Discharge</a></li>
 				<li><a href='https://cds-hooks.hl7.org/hooks/appointment-book/STU1/appointment-book/'>Appointment Book</a></li>
 			</ul>"
+		},
+                {
+			"version": "1.0.0-ballot",
+			"path": "https://cds-hooks.hl7.org/ballots/2023Sep/",
+			"status": "ballot",
+			"sequence": "STU1",
+			"fhirversion": "4.0.1",
+			"date": "2023-08-01",
+			"desc": "CDS Hooks Library Hook Maturity Ballot",
+			"changes": "Update hook maturity per CDS Hooks Maturity Model."
 		}
 	]
 }`;
